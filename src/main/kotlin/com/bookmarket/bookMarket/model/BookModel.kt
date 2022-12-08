36f0db3f4ee82ea01aadef3f5540c83f49aeec49
k3vinrich4rd <kevinrichardve@gmail.com>
2022-12-08 12:36:30 -0300

    Inserindo a criação da entidade 'book' e o seu relacionamento com 'customer'

---
+++ src/main/kotlin/com/bookmarket/bookMarket/model/BookModel.kt
@@ -0,0 +1,28 @@
+package com.bookmarket.bookMarket.model
+
+import com.bookmarket.bookMarket.enums.BookStatus
+import java.math.BigDecimal
+import javax.persistence.*
+
+@Entity(name = "book")
+data class BookModel(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @Column(name = "name_book", length = 70, nullable = true)
+    var name: String,
+
+    @Column(name = "price_book", nullable = true, precision = 10, scale = 2)
+    var price: BigDecimal,
+
+    @Column(name = "status_book", length = 70, nullable = true)
+    @Enumerated(EnumType.STRING) //Texto
+    var status: BookStatus?,
+
+    @ManyToOne //Muitos para um
+    @JoinColumn(name = "customer_id")
+    var customerModel: CustomerModel? = null
+
+)

f5d219561842489ba591ce6b273997f6d334778e
k3vinrich4rd <kevinrichardve@gmail.com>
2022-12-18 21:16:01 -0300

    Inserindo utilização de beans e swagger documentation

---
+++ src/main/kotlin/com/bookmarket/bookMarket/config/SwaggerConfig.kt
@@ -0,0 +1,36 @@
+package com.bookmarket.bookMarket.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2 //Indica que o spring ative o swagger presente na aplicação
+
+/*
+/indicando para o spring que vamos criar um bean
+e que ele observar todos os métodos dessa classe
+*/
+class SwaggerConfig {
+
+    @Bean //Feijão
+    fun api(): Docket = Docket(DocumentationType.SWAGGER_2) //Tipo de documentação
+        .select() //Selecione
+        .apis(RequestHandlerSelectors.basePackage("com.bookmarket.bookMarket.controller")) //Tipo de selector com package onde se encontra o os controllers
+        .paths(PathSelectors.any()) //para selecionar todos os caminhos (any = qualquer)
+        .build() //Retorna um docket
+
+            //Para mudar o nome a descrição da documentação
+        .apiInfo(
+            ApiInfoBuilder()
+                .title("Book Market")
+                .description("Book Market API")
+                .build()
+        )
+}

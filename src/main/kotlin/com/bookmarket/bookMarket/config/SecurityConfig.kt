a67d3ed00f6078494a3d398243efc20d801fafcb
k3vinrich4rd <kevinrichardve@gmail.com>
2022-12-20 08:19:16 -0300

    Preparando o ambiente de autenticação das requisições

--- src/main/kotlin/com/bookmarket/bookMarket/config/SecurityConfig.kt
+++ src/main/kotlin/com/bookmarket/bookMarket/config/SecurityConfig.kt
@@ -1,14 +1,48 @@
+@file:Suppress("DEPRECATION")
+
 package com.bookmarket.bookMarket.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity // == Habilitar segurança da web
+class SecurityConfig() {
+
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.cors().and().csrf().disable() //Cors = núcleos
+        //Estamos falando aqui que as requisições vão ser independentes
+        http.authorizeRequests().anyRequest().authenticated() //Todas as requisições têm que estar autenticadas
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        return http.build()
+    }
 
     @Bean //Para criptografar a senha
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder()
     }
 }
+
+
+/*  outra forma de implementação
+
+  override fun configure(http: HttpSecurity) { //Cors = núcleos
+//        http.cors().and().csrf().disable() //Para desabilitar o csrf
+//        //Estamos falando aqui que as requisições vão ser independentes
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+   }
+
+    @Bean //Para criptografar a senha
+    fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+*/

7f9cafc73d7c5b36f14e2cb544422d3744645ef1
k3vinrich4rd <kevinrichardve@gmail.com>
2022-12-20 08:32:45 -0300

    Inserindo a liberação de algumas rotas de requisição e bloqueio de outras

--- src/main/kotlin/com/bookmarket/bookMarket/config/SecurityConfig.kt
+++ src/main/kotlin/com/bookmarket/bookMarket/config/SecurityConfig.kt
@@ -4,6 +4,7 @@ package com.bookmarket.bookMarket.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -15,11 +16,22 @@ import org.springframework.security.web.SecurityFilterChain
 class SecurityConfig() {
 
 
+    //Assim fica mais fácil e moldável, pois caso queira colocar mais uma liberação,
+    //basta adicionar na lista (Tirando a necessidade de ficar escrevendo na mão)
+    private val publicPostMatchers = arrayOf(
+        "/customers"
+    )
+
+
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.cors().and().csrf().disable() //Cors = núcleos
         //Estamos falando aqui que as requisições vão ser independentes
-        http.authorizeRequests().anyRequest().authenticated() //Todas as requisições têm que estar autenticadas
+        http.authorizeRequests()
+            //Para liberar o customer se cadastrar
+                //O '*' modulariza e passa todos os registros da lista como se fossem varias Strings
+            .antMatchers(HttpMethod.POST, *publicPostMatchers).permitAll() //Aceita infinitos parâmetros
+            .anyRequest().authenticated() //Todas as requisições têm que estar autenticadas
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
         return http.build()
@@ -32,7 +44,7 @@ class SecurityConfig() {
 }
 
 
-/*  outra forma de implementação
+/*  outra forma de implementação (Antiga e descontinuada)
 
   override fun configure(http: HttpSecurity) { //Cors = núcleos
 //        http.cors().and().csrf().disable() //Para desabilitar o csrf
